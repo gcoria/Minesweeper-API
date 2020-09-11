51fbb88702be88b9e26d2440d3f097151494d243
gcoria <gaston.coria@mercadolibre.com>
2020-09-11 01:56:30 -0300

    [User] add user controller and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render :index, status: :ok, formats: :json
+  end
+
+  def show
+  	@user = User.find(params[:id])
+    render :show, formats: :json, status: :ok
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  private  	
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
