0881cd3a3f3cb3825200b988e0c0edd79bf42aee
gcoria <gaston.coria@mercadolibre.com>
2020-09-13 17:18:33 -0300

    [Fix] api-client typo

--- minesweeper-client/minesweeper_client.py
+++ minesweeper-client/minesweeper_client.py
@@ -1,43 +1,41 @@
 import requests
 
 class MinesweeperClient:
-  BASE_URL = 'https://glacial-oasis-14537.herokuapp.com'
+    BASE_URL = 'https://glacial-oasis-14537.herokuapp.com'
 
-  def createUser(self, username, password):
-    return requests.post(
-      url=self.BASE_URL+'/users',
-      json={
-        "username": username,
-        "password": password
+    def createUser(self, username, password):
+        return requests.post(
+            url=self.BASE_URL+'/users',
+            json={
+                "username": username,
+                "password": password
             }
-    ).json()
+        ).json()
 
-  def getUser(self, userId):
-    return requests.get(url=self.BASE_URL+'/users/'+str(userId)).json()
+    def getUser(self, userId):
+        return requests.get(url=self.BASE_URL+'/users/'+str(userId)).json()
 
-  def listUsers(self):
-    return requests.get(url=self.BASE_URL+'/users').json()
+    def listUsers(self):
+        return requests.get(url=self.BASE_URL+'/users').json()
 
-    
-  def createGame(self, userId, rows, columns, mines):
-    return requests.post(
-      url=self.BASE_URL+'/users/'+str(userId)+'/games',
-      json={
-        "rows": rows,
-        "columns": columns,
-        "mines": mines
-      }
-    ).json()
+    def createGame(self, userId, rows, columns, mines):
+        return requests.post(
+            url=self.BASE_URL+'/users/'+str(userId)+'/games',
+            json={
+                "rows": rows,
+                "columns": columns,
+                "mines": mines
+            }
+        ).json()
 
-  def listUserGames(self, userId):
-    return requests.get(url=self.BASE_URL+'/users/'+str(userId)+'/games').json()
+    def listUserGames(self, userId):
+        return requests.get(url=self.BASE_URL+'/users/'+str(userId)+'/games').json()
   
-  def getGame(self, userId, gameId):
-    return requests.get(url=self.BASE_URL+'/users/'+str(userId)+'/games/'+str(gameId)).json()
+    def getGame(self, userId, gameId):
+        return requests.get(url=self.BASE_URL+'/users/'+str(userId)+'/games/'+str(gameId)).json()
 
-  def markCell(self, userId, gameId, x_axis, y_axis):
-    return requests.put(url=self.BASE_URL+'/users/'+str(userId)+'/games/'+str(gameId)+'/mark/'+str(x_axis)+'/'str(y_axis)).json()
+    def markCell(self, userId, gameId, x_axis, y_axis):
+        return requests.put(url=self.BASE_URL+'/users/'+str(userId)+'/games/'+str(gameId)+'/mark/'+str(x_axis)+'/'+str(y_axis)).json()
 
-  def revealCell(self, userId, gameId, x_axis, y_axis):
-    return requests.put(url=self.BASE_URL+'/users/'+str(userId)+'/games/'+str(gameId)+'/reveal/'+str(x_axis)+'/'str(y_axis)).json()  
- 
+    def revealCell(self, userId, gameId, x_axis, y_axis):
+        return requests.put(url=self.BASE_URL+'/users/'+str(userId)+'/games/'+str(gameId)+'/reveal/'+str(x_axis)+'/'+str(y_axis)).json()
